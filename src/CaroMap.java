bf793b6ef53fc36440568ae6de783ad332c63a58
datdta95 <datdta95@gmail.com>
2018-08-20 17:32:13 +0700

    Vẽ được bản đồ cho 2 người chơi liên tiếp nhưng chưa kiểm tra trùng tọa độ

--- src/CaroMap.java
+++ src/CaroMap.java
@@ -4,8 +4,56 @@ public class CaroMap {
     public static void main(String[] args) {
         int numberOfRows = getNumberOfRows() * 2 + 1;
         int numberOfColumns = getNumberOfColumns() * 2 + 1;
-        displayCaroMap(drawCaroMap(numberOfRows,numberOfColumns));
+        char[][] array = drawCaroMap(numberOfRows, numberOfColumns);
+        displayCaroMap(array);
+        for (int i = 0; i < 100; i++) {
+            int coordinatesOfRow = (getCoordinatesOfRow() - 1) * 2 + 1;
+            int coordinatesOfColumn = (getCoordinatesOfColumn() - 1) * 2 + 1;
+            if (i % 2 == 0) {
+                displayCaroMap(drawCaroMapWithValueO(coordinatesOfRow, coordinatesOfColumn, array));
+            } else {
+                displayCaroMap(drawCaroMapWithValueX(coordinatesOfRow, coordinatesOfColumn, array));
+            }
+
+        }
     }
+
+    private static char[][] drawCaroMapWithValueO(int coordinatesOfRow, int coordinatesOfColumn, char[][] array) {
+        char[][] arrayDrawed = array;
+        for (int i = 0; i < arrayDrawed.length; i++) {
+            for (int j = 0; j < arrayDrawed[i].length; j++) {
+                if (i == coordinatesOfRow && j == coordinatesOfColumn) {
+                    arrayDrawed[i][j] = 'o';
+                }
+            }
+        }
+        return arrayDrawed;
+    }
+
+    private static char[][] drawCaroMapWithValueX(int coordinatesOfRow, int coordinatesOfColumn, char[][] array) {
+        char[][] arrayDrawed = array;
+        for (int i = 0; i < arrayDrawed.length; i++) {
+            for (int j = 0; j < arrayDrawed[i].length; j++) {
+                if (i == coordinatesOfRow && j == coordinatesOfColumn) {
+                    arrayDrawed[i][j] = 'x';
+                }
+            }
+        }
+        return arrayDrawed;
+    }
+
+    private static int getCoordinatesOfColumn() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào tọa độ cột cho bước đi:");
+        return scanner.nextInt();
+    }
+
+    private static int getCoordinatesOfRow() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào tọa độ hàng cho bước đi:");
+        return scanner.nextInt();
+    }
+
 
     private static int getNumberOfRows() {
         Scanner scanner = new Scanner(System.in);
@@ -28,7 +76,7 @@ public class CaroMap {
         }
     }
 
-    private static char[][] drawCaroMap(int numberOfRows,int numberOfColumns) {
+    private static char[][] drawCaroMap(int numberOfRows, int numberOfColumns) {
         char[][] arrayCaroMap = new char[numberOfRows][numberOfColumns];
         for (int i = 0; i < arrayCaroMap.length; i++) {
             for (int j = 0; j < arrayCaroMap[i].length; j++) {
